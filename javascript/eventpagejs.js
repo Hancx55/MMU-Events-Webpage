cb3230c4e580e1fc48571e69be80ddcff5a5ce3e
Hancx55 <taylorlongworthh@gmail.com>
2026-03-11 13:20:37 +0000

    nav on all pages, eventpage now allows events to be bookable and displays them

---
+++ javascript/eventpagejs.js
@@ -0,0 +1,82 @@
+//example events to input for now
+const modal = document.getElementById("event-details");
+const eventTitle = document.getElementById("event-name");
+const eventDate = document.getElementById("event-date");
+const eventDesc = document.getElementById("event-description");
+const bookBtn = document.getElementById("bookevent");
+const bookingStatus = document.getElementById("bookingStatus");
+const closeBtn = document.getElementById("close-event");
+
+let selectedEvent = null;
+
+const events = Array.from({ length:32 }, (_, i) => ({
+    id: i+1,
+    name: `Event ${i+1}`,
+    shortdesc: 'short desc goes here',
+    description: 'full desc goes here',
+    datetime: "2026-05-05 10:47",
+    attendees: []
+}))
+
+//switch content between pages
+let current=1;
+const PerPage = 10;
+
+document.getElementById("prev-page").onclick = () => {
+    if (current>1) {
+        current--;
+        render();
+    }
+}
+
+document.getElementById("next-page").onclick = () => {
+    if (current< Math.ceil(events.length/PerPage)) {
+        current++;
+        render();
+    }
+}
+
+function render() {
+    const start = (current - 1) * PerPage;
+    const pageEvents = events.slice(start, start + PerPage);
+
+    for (let i = 0; i < 10; i++) {
+        const box = document.getElementById(`e${i + 1}`);
+        const event = pageEvents[i];
+
+        if (!event) {
+            box.style.display = "none";
+            continue;
+        }
+
+        box.style.display = "block";
+        box.innerHTML = `
+            <h3>${event.name}</h3>
+            <p>${event.shortdesc}</p>
+            <p><strong>${event.datetime}</strong></p>
+        `;
+
+        box.onclick = () => openevent(event);
+    }
+
+    document.getElementById("page-info").textContent = `Page ${current}`;
+}
+
+function openevent(event) {
+    selectedEvent = event;
+    eventTitle.textContent = event.name;
+    eventDesc.textContent = event.description;
+    eventDate.textContent = "Date/Time: " + event.datetime;
+    bookingStatus.textContent = "";
+    modal.style.display = "flex";
+}
+
+bookBtn.onclick = () => {
+    selectedEvent.attendees.push("Username");
+    bookingStatus.textContent = "You have booked this event!";
+    bookingStatus.style.color = "green";
+};
+
+closeBtn.onclick = () => {
+    modal.style.display = "none";
+};
